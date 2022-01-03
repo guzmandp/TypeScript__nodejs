743082c2a25bc7fdf47d3e3a25f9e91bec088d9c
guzmandp <guzmandp@gmail.com>
2022-01-03 18:05:02 -0300

    update folder uploads

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import routes from './routes'
+import path from 'path';
 
 const port = 8000
 
 const app = express();
 
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 
 app.listen(port, () => {
